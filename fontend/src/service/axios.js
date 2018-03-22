393e4c25d384cd19b21850f310b0f11fa0245ad8
boatnoodle <nattasit31@hotmail.com>
2018-03-22 14:14:30 +0700

    chagne ip axios for develop at home

--- fontend/src/service/axios.js
+++ fontend/src/service/axios.js
@@ -1,9 +1,57 @@
 import axios from 'axios'
-import ip from 'ip'
-const Ip = ip.address()
+/**
+ * Get the user IP throught the webkitRTCPeerConnection
+ * @param onNewIP {Function} listener function to expose the IP locally
+ * @return undefined
+ */
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    //compatibility for firefox and chrome
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
 
-const instance = axios.create({
-    baseURL: `http://${Ip}:3000/api/`,
-})
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
 
-export default instance
+     //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer().then(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp, noop, noop);
+    }).catch(function(reason) {
+        // An error occurred, so handle the failure to connect
+    });
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+}
+
+// Usage
+
+const  instance = getUserIP(function(ip){
+    axios.create({
+        baseURL: `http://${ip}:3000/api/`,
+    })
+ });
+//  console.log(ip)
+//  const instance = axios.create({
+//     baseURL: `http://${ip}:3000/api/`,
+// })
+ export default instance
+
