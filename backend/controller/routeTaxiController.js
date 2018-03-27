836ddbb3d69892e96ceeaa2d39f801fc5d2836e4
boatnoodle <nattasit31@hotmail.com>
2018-03-27 22:42:44 +0700

    sort name route

--- backend/controller/routeTaxiController.js
+++ backend/controller/routeTaxiController.js
@@ -14,6 +14,7 @@ module.exports = {
     },
     getAllRoute: (req,res,next) => {
         RouteTaxi.find()
+        .sort('nameRoute') // sort by name route
         .exec()
         .then(route => {
             res.status(200).json(route)
