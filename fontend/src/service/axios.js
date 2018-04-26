cc6bc637d8ba35d41909f765a17645ff5aaf1a5f
boatnoodle <nattasit31@hotmail.com>
2018-04-26 11:54:53 +0700

    change css print

--- fontend/src/service/axios.js
+++ fontend/src/service/axios.js
@@ -3,8 +3,8 @@ import Ip from 'ip'
 
 const ip = Ip.address()
 const instance = axios.create({
-    // baseURL: `http://192.168.1.145:3000/api/`,
-    baseURL: `http://${ip}:3000/api/`
+    // baseURL: `http://192.168.1.4:3000/api/`,
+    baseURL: `http://192.168.1.4:3000/api/`
     
 })
  export default instance
