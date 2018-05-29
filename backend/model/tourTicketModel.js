fa07a1cfbf9d9cee3a251a062b553d47dfba73b2
boatnoodle <nattasit31@hotmail.com>
2018-05-29 09:16:51 +0700

    update modal tourTicket

--- backend/model/tourTicketModel.js
+++ backend/model/tourTicketModel.js
@@ -2,33 +2,33 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const tourTicketSchema = new Schema({
-    _id: {type: String, required: true},
-    name: {type: String, required: true},
-    agent: {type: String, required: true},
-    tour:{
-            nameTour: {
-                type: String,
-                required: true
-            },
-            priceAdult: {
-                type: Number,
-                required: true
-            },
-            priceChild: {
-                type: Number,
-                required: true
-            },
-            netPriceAdult: {
-                type: Number,
-                require: true
-            },
-            netPriceChild: {
-                type: Number,
-                require: true
-            }
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    agent: { type: Schema.Types.ObjectId, ref: 'Tour' },
+    tour: {
+        nameTour: {
+            type: String,
+            required: true
+        },
+        priceAdult: {
+            type: Number,
+            required: true
+        },
+        priceChild: {
+            type: Number,
+            required: true
+        },
+        netPriceAdult: {
+            type: Number,
+            require: true
+        },
+        netPriceChild: {
+            type: Number,
+            require: true
+        }
     },
-    amountAdult: {type: Number, required: true},
-    amountChild: {type: Number},
+    amountAdult: { type: Number, required: true },
+    amountChild: { type: Number },
     // total: {type: Number, requried: true},
     voucher: String,
     remark: String,
@@ -38,4 +38,4 @@ const tourTicketSchema = new Schema({
     }
 })
 
-module.exports = mongoose.model('TourTicket',tourTicketSchema)
+module.exports = mongoose.model('TourTicket', tourTicketSchema)
