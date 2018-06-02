2d63262c2c0bd6a80b4284fb32956bfdaab25321
boatnoodle <nattasit31@hotmail.com>
2018-06-02 16:51:43 +0700

    update title bill for tour

--- fontend/src/components/setPrint.vue
+++ fontend/src/components/setPrint.vue
@@ -1,23 +1,35 @@
 <template>
   <div>
       <h3><i class="fa fa-print"></i> ตั้งค่าการปริ้น</h3><hr>
-      <div class="row" style="margin-top: 20px;">
-          <div class="col-sm-6 offset-sm-3">
-            <form v-on:submit.prevent="submit">
-                <div class="form-group">
-                    <label>1. เปิด/ปิด การออกใบเสร็จ</label>
-                    <input v-model="data.statusPrint" class="form-control" style="width: auto;" type="checkbox">
+        <form v-on:submit.prevent="submit">
+            <div class="row" style="margin-top: 20px;">
+                <div class="col-sm-6">
+                        <h5>Taxi</h5>
+                        <div class="form-group">
+                            <label>1. เปิด/ปิด การออกใบเสร็จ</label>
+                            <input v-model="data.statusPrint" class="form-control" style="width: auto;" type="checkbox">
+                        </div>
+                        <div class="form-group">
+                            <label>2. จำนวนครั้งที่ออกใบเสร็จ</label>
+                            <input type="number" v-model="data.amountPrint" class="form-control" placeholder="ระบุจำนวนครั้งที่ต้องการออกใบเสร็จ">
+                        </div>
                 </div>
-                <div class="form-group">
-                    <label>2. จำนวนครั้งที่ออกใบเสร็จ</label>
-                    <input type="number" v-model="data.amountPrint" class="form-control" placeholder="ระบุจำนวนครั้งที่ต้องการออกใบเสร็จ">
+                <div class="col-sm-6">
+                        <h5>Tour</h5>
+                        <div class="form-group">
+                            <label>1. เปิด/ปิด การออกใบเสร็จ</label>
+                            <input v-model="data.statusPrintTour" class="form-control" style="width: auto;" type="checkbox">
+                        </div>
+                        <div class="form-group">
+                            <label>2. จำนวนครั้งที่ออกใบเสร็จ</label>
+                            <input type="number" v-model="data.amountPrintTour" class="form-control" placeholder="ระบุจำนวนครั้งที่ต้องการออกใบเสร็จ">
+                        </div>
+                        <div class="pull-right">
+                            <button type="submit" class="btn btn-info">บันทึก</button>
+                        </div>
                 </div>
-                <div>
-                    <button type="submit" class="btn btn-info">บันทึก</button>
-                </div>
-            </form>
-          </div>
-      </div>
+            </div>
+        </form>
   </div>
 </template>
 <script>
@@ -27,7 +39,9 @@ export default {
         return {
             data: {
                statusPrint: true,
-               amountPrint: 1
+               amountPrint: 1,
+               statusPrintTour: true,
+               amountPrintTour: 1
             }
         }
     },
@@ -49,6 +63,8 @@ export default {
                 const obj = this.$store.getters.getPrintSetting
                 this.data.statusPrint = obj.statusPrint
                 this.data.amountPrint = obj.amountPrint
+                this.data.statusPrintTour = obj.statusPrintTour
+                this.data.amountPrintTour = obj.amountPrintTour
                 // console.log(obj)
                 return obj
             })
